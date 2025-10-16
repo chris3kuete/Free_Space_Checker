db306408d51943b891b3df6b1fb52dccbfa2bdb5
chris3kuete <chriskuete@yahoo.fr>
2025-10-16 12:50:38 -0700

    first commit

---
+++ Free_Space_Checker.ps1
@@ -0,0 +1,12 @@
+# Ask the user for the name or location of the drive
+
+$freeSpace = [Math]::Round((Get-CimInstance -ClassName Win32_LogicalDisk | Where-Object DeviceID -eq 'C:').FreeSpace / 1GB)
+
+if ($freeSpace -eq $null) {
+    Write-Host "there is no free space"
+    <# Action to perform if the condition is true #>
+}elseif($freeSpace -gt 100){
+    Write-Host " The amount of free space is $freeSpace, you still have plenty of space available"
+}else{
+    Write-Host "Time to delete some files because you only have $freeSpace"
+}
